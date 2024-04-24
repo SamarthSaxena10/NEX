34ff1d55b9504e3d5480905dcd1007473e3232c7
Samarth <sam@Samarths-MacBook-Air.local>
2024-04-24 12:47:27 +0530

    Addition for Files in root folder

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,26 @@ import express from "express";
 import cors from "cors";
 import simpleGit from "simple-git";
 import { generate } from "./utils";
+import path from 'path'
+import { getAllFiles } from "./file";
 
 const app = express();
 app.use(cors())
 app.use(express.json());
+console.log(path.join(__dirname,`output/randomstring`));
 
 app.post("/deploy", async (req, res) => {
     const repoUrl = req.body.repoUrl;
     const id = generate()
-    await simpleGit().clone(repoUrl, `output/${id}`)
-    res.json({})
+    await simpleGit().clone(repoUrl, path.join(__dirname,`output/${id}`))
+
+    const files = getAllFiles(path.join(__dirname,`output/${id}`));
+    console.log(files)
+
+
+    res.json({
+        id: id
+    })
 });
 
 app.listen(3000, () => {
