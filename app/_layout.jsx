dff3d6abae6f285dd1f2b3b182e6fb6ad82bb87e
Samarth <sam@Samarths-MacBook-Air.local>
2024-04-25 11:52:41 +0530

    Launching of react native

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+
+
+
+const _layout = () => {
+  return (
+   <Slot/>
+  )
+}
+
+export default _layout
+
